62dcbaa9251966137aa4240a4207fc06cc95a68a
ganeshbirajdar286 <ganeshbirajdar286@gmail.com>
2025-11-11 10:03:59 +0530

    add versel.json

--- frontend/src/pages/services/url.services.js
+++ frontend/src/pages/services/url.services.js
@@ -1,7 +1,6 @@
 import axios from "axios";
  const apiUrl= `${import.meta.env.VITE_API_URL}/api`
 
- const getToken=()=>localStorage.getItem("auth-token");
 
 export const axiosInstance = axios.create({
   baseURL:apiUrl,
